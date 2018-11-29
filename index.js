09195a8245e18fbd73dbcae7b4db6e9de9dbfa13
wingsico <wingsico@gmail.com>
2018-11-29 16:25:15 +0800

    update: export translator

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const Grammer = require('./lib/class/Grammer');
+const { formatGsString, formatGsArray, formatOutputGrammer } = require('./lib/utils');
+const removeLeftRecursion = require('./lib/components/removeLeftRecursion');
+const removeLeftDivisor = require('./lib/components/removeLeftDivisor');
+const simplify = require('./lib/components/simplify');
+
+function checkoutConfig(config = {}) {
+  if (!config.inputPath) {
+    throw new Error("lack of required config attributes: inputPath")
+  }
+}
+
+(async () => {
+  exports.translator = function (customConfig = {}) {
+    const config = Object.assign({
+      inputPath: '',
+      displayProcess: false,
+      outputPath: './output.txt',
+      startSymbol: 'S',
+    }, customConfig);
+
+    checkoutConfig(config);
+
+    const { inputPath, displayProcess, outputPath, startSymbol } = config;
+
+    const gs = formatGsArray(formatGsString(fs.readFileSync(inputPath, 'utf-8')));
+
+    const grammer = new Grammer(startSymbol, gs);
+    if (displayProcess) {
+      console.log('===== (1) start creating grammer =====');
+      formatOutputGrammer(grammer);
+      console.log('===== (1) completed =====');
+      console.log('===== (2) start eliminating left recursion =====');
+      removeLeftRecursion(grammer);
+      formatOutputGrammer(grammer);
+      console.log('===== (2) completed =====');
+      console.log('===== (3) start simplifying grammer =====');
+      simplify(grammer);
+      formatOutputGrammer(grammer);
+      console.log('===== (3) completed =====');
+      console.log('===== (4) start extracting left common factor  =====')
+      removeLeftDivisor(grammer);
+      formatOutputGrammer(grammer);
+      console.log('===== (4) completed =====');
+    } else {
+      removeLeftRecursion(grammer);
+      simplify(grammer);
+      removeLeftDivisor(grammer);
+    }
+
+    const productions = grammer.getProductions();
+    if (!!outputPath) {
+      fs.writeFileSync(outputPath, Object.keys(productions).map(key => `${key}->${productions[key]};`).join("\n"));
+    }
+
+    return productions;
+  }
+})()
