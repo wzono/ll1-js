3b3e585c5853ad7987feedfe47120fe0aff7d293
wingsico <wingsico@gmail.com>
2018-11-29 16:26:05 +0800

    test

---
+++ test/index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const filePath = path.resolve(__dirname, './g.txt');
+const { translator } = require('../index');
+(async () => {
+  const config = {
+    displayProcess: true,
+    inputPath: filePath,
+    outputPath: './output.txt',
+    startSymbol: 'S',
+  }
+
+  const ll1Produtions = translator(config);
+})()
